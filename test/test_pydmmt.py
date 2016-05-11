6c5d211f9fc44a6285504835654c1a365c8b4696
Emanuele Mason <lordmzn@gmail.com>
2016-05-11 11:51:53 +0200

    written code of first test

--- test/test_pydmmt.py
+++ test/test_pydmmt.py
@@ -5,15 +5,37 @@ import pytest
 from pydmmt import pydmmt
 
 
-class TestPydmmt(object):
+def test_pydmmt_as_a_calc():
+    """ ./pyDMMT sum.py """
+    from subprocess import Popen, PIPE, STDOUT
+    p = Popen(["./pyDMMT", "sum.py"], stdin=PIPE, stdout=PIPE, stderr=STDOUT)
+    output = p.communicate("3 2")
+    assert output == 5
+    # None means it's still running; negative returncode means terminated with
+    # signal -r
+    assert p.returncode >= 0
 
-    @classmethod
-    def setup_class(cls):
-        pass
 
-    def test_something(self):
-        assert pydmmt.step(5) == 6
+def test_fibonacci():
+    """ ./pyDMMT fibonacci.py """
+    execute pyDMMT fibonacci (contains already horizon + init conditions)
+    read stdout
+    assert stdout == 144
+    assert process(pyDMMT) == dead
 
-    @classmethod
-    def teardown_class(cls):
-        pass
+
+def test_fibonacci_simulation_data():
+    """ ./pyDMMT fibonacci.py """
+    ask simulation_data
+    execute pyDMMT fibonacci (contains already horizon + init conditions)
+    read stdout
+    assert stdout == 144
+    assert process(pyDMMT) == dead
+    assert exist(sim_data_file)
+    read sim_data_file
+    assert n_data == expected
+
+
+def test_leslie_dataset_file():
+    """ ./pyDMMT -d dataset.csv leslie.py """
+    pass
